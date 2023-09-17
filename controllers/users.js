64419577f0f2e83e02d967a65bb8a76b268287f8
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-09-17 17:38:40 +0300

    add try/catch to login users controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,34 +5,38 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
 const login = async (req, res, next) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: "Пожалуйста, заплните обязательные поля" });
-  }
-
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  });
-
-  const isPasswordCorrect =
-    user && (await bcrypt.compare(password, user.password));
-
-  if (user && isPasswordCorrect && secret) {
-    return res.status(200).json({
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+  try {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Пожалуйста, заплните обязательные поля" });
+    }
+  
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
     });
-  } else {
-    return res.status(400).json({
-      message: "Неверно введен логин или пароль",
-    });
+  
+    const isPasswordCorrect =
+      user && (await bcrypt.compare(password, user.password));
+  
+    if (user && isPasswordCorrect && secret) {
+      return res.status(200).json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+      });
+    } else {
+      return res.status(400).json({
+        message: "Неверно введен логин или пароль",
+      });
+    }
+  } catch {
+    res.status(500).json({ message: "Что-то пошло не так" });
   }
 };
 
