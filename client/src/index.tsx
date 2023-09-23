b571938af1139833ef4b08db3d416453b06b38f9
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-09-23 18:13:04 +0300

    add router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { Paths } from './paths';
+import { Login } from './pages/login';
+import { Register } from './pages/register';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Employees</h1>
+  },
+  {
+    path: Paths.login,
+    element: <Login/>
+  },
+  {
+    path: Paths.register,
+    element: <Register/>
+  }
+])
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello, World!</h1>
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
